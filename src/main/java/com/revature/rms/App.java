e2a93328216d4ffa43bf1ba17293c2ded777e218
NoahThacker31 <31545990+NoahThacker31@users.noreply.github.com>
2019-03-25 11:33:35 -0400

    Swagger UI

--- src/main/java/com/revature/rms/App.java
+++ src/main/java/com/revature/rms/App.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @SpringBootApplication
 @EnableEurekaServer
 public class App {
